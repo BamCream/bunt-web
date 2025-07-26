7b5414a68ece74d3059ce1a222abfa9d6cca5dec
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2025-07-26 19:18:47 +0900

    feat: goods dict

---
+++ src/components/ui/dict/goodCards/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './style.scss'
+
+interface GoodsCardProps {
+  imageUrl: string
+  name: string
+  isLocked: boolean
+}
+
+const GoodsCard = ({ imageUrl, name, isLocked }: GoodsCardProps) => {
+  return (
+    <div className={`goodsContainer ${isLocked ? 'locked' : ''}`}>
+      <img src={imageUrl} alt={name} />
+      <div className="goodsInfo">
+        <h3>{name}</h3>
+        <p>{isLocked ? '🔒 열지 못했습니다.' : '🔓 잠금해제 했습니다.'}</p>
+      </div>
+    </div>
+  )
+}
+
+export default GoodsCard
