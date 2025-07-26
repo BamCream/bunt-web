8b51ba0d7e2ab56fbc58f82f529f27d4ccb5cc75
chaeyn <chaulfe@gmail.com>
2025-07-26 21:52:06 +0900

    feat: 도감 페이지 뷰 수정

--- src/pages/dex/index.tsx
+++ src/pages/dex/index.tsx
@@ -2,36 +2,47 @@ import './style.scss'
 import { Select } from '@b1nd/b1nd-dodamdodam-ui'
 import { TEAM_LIST } from 'src/constants/team/team.constants'
 import { useDex } from 'src/hooks/dict/useDict'
-import GoodsCard from '../../components/ui/dex/goodCards'
+import GoodsCard from 'src/components/ui/dex/goodCards'
 
 const Dex = () => {
   const { isSelected, setIsSelected } = useDex()
+
+  const categories = ['모자', '유니폼', '기타']
+
   return (
-    <div className="pageTemplate">
-      <div className="titleWrap">
-        <h1 className="title">도감</h1>
-        <button className="registCodeBtn">코드 등록</button>
+    <div className="dexPage">
+      <div className="dexHeader">
+        <h1>도감</h1>
+        <button className="codeButton">코드 등록</button>
       </div>
-      <div className="goodsWrap">
-        <div className="categoryWrap">
-          <Select
-            items={TEAM_LIST}
-            value={isSelected}
-            onChange={setIsSelected}
-            zIndex={2}
-            customStyle={{ width: '10%' }}
-          />
-        </div>
-        <div className="goodsListWrap">
-          {Array.from({ length: 30 }).map((_, index) => (
-            <GoodsCard
-              key={index}
-              imageUrl="https://kbomarket.com/web/product/big/20250320/97ca84111d5abbf59f245c4d24289547.jpg"
-              name=""
-              isLocked={false}
-            />
+
+      <div className="filterSection">
+        <div className="categoryButtons">
+          {categories.map((category) => (
+            <button key={category} className="categoryBtn">
+              {category}
+            </button>
           ))}
         </div>
+
+        <Select
+          items={TEAM_LIST}
+          value={isSelected}
+          onChange={setIsSelected}
+          zIndex={2}
+          customStyle={{ width: '140px' }}
+        />
+      </div>
+
+      <div className="goodsGrid">
+        {Array.from({ length: 12 }).map((_, index) => (
+          <GoodsCard
+            key={index}
+            imageUrl="https://kbomarket.com/web/product/big/20250320/97ca84111d5abbf59f245c4d24289547.jpg"
+            name="Men's Los Angeles Dodgers Hat"
+            isLocked={index % 3 === 0}
+          />
+        ))}
       </div>
     </div>
   )
