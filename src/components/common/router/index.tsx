b7dd37d404f49546cfb31fe967ba41e0a254e38d
chaeyn <chaulfe@gmail.com>
2025-07-27 01:06:24 +0900

    feat: 홈 페이지

--- src/components/common/router/index.tsx
+++ src/components/common/router/index.tsx
@@ -4,13 +4,16 @@ import Dex from 'src/pages/dex'
 import Record from 'src/pages/record'
 import Upload from 'src/pages/upload'
 import Profile from 'src/pages/profile'
+import Home from 'src/pages/home'
+import PostDetail from 'src/pages/post'
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<SidebarLayout />}>
-          <Route index element={<div>Home Page</div>} />
+          <Route index element={<Home />} />
+          <Route path="post/:id" element={<PostDetail />} />
           <Route path="dex" element={<Dex />} />
           <Route path="record" element={<Record />} />
           <Route path="upload" element={<Upload />} />
