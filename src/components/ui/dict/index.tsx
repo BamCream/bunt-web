beefbdfcf3b4e4d9336b35cb3c3ad18530d74a81
전민찬 <132974029+wjzlskxk@users.noreply.github.com>
2025-07-26 18:30:24 +0900

    chore: 중간 커밋

--- src/components/ui/dict/index.tsx
+++ src/components/ui/dict/index.tsx
@@ -5,7 +5,15 @@ import Layout from 'src/components/common/layout'
 const Dict = () => {
   return (
     <Layout>
-      <div className="pageTemplate"></div>
+      <div className="pageTemplate">
+        <div className="titleWrap">
+          <h1 className="title">도감</h1>
+          <button className="registCodeBtn">코드 등록</button>
+        </div>
+        <div className="goodsWrap">
+          
+        </div>
+      </div>
     </Layout>
   )
 }
